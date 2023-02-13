72b756a0548525cfe1851712ddda0ce9eecdfe7e
cokoyoh <charlesokoyoh@gmail.com>
2023-02-13 11:21:55 +0300

    Add a curry function

--- src/index.js
+++ src/index.js
@@ -8,5 +8,19 @@ const not = (predicate) => {
 
 const isLongEnough = not(isShortEnough);
 
+const curry = (fn, arity = fn.length) => {
+  return (function nextCurried(previousArgs) {
+    return function curried(...nextArgs) {
+      const args = [...previousArgs, nextArgs];
+
+      if (args.length >= arity) {
+        return fn(args)
+      }
+
+      return nextCurried(args)
+    }
+  })([])
+}
+
 console.log({ isLong: isLongEnough('this is a long string') });
 console.log({ isNotLong: isLongEnough('not') });
