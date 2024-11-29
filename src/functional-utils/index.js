293b1898b7546787a5d8b6272394edf9ee6b9070
cokoyoh <charlesokoyoh@gmail.com>
2024-11-29 09:00:04 +0300

    Add spreadArgs, partial to the arsenal of functional programming

---
+++ src/functional-utils/index.js
@@ -0,0 +1,32 @@
+const guard = (fn) => (arg) => {
+  return arg ? fn(arg) : arg;
+};
+
+const compose = (...fns) => {
+  return [...fns].reverse().reduce((f1, f2) => {
+    return (...args) => f2(f1(...args));
+  });
+};
+
+const reverseArgs = (fn) => {
+  return (...args) => fn(...args.reverse());
+};
+
+const pipe = reverseArgs(compose);
+
+const trace = (label) => (value) => {
+  console.log({ [label]: value });
+  return value;
+};
+
+const composeMethod = (method) => (...fns) => [...fns].reduce(
+  (f, g) => (...args) => g(...args)[method](f)
+);
+
+const partial = (fn, ...presetArgs) => {
+  return (...laterArfs) => fn(...presetArgs, ...laterArfs)
+}
+
+const spreadArgs = (fn) => (args) => fn(...args)
+
+module.exports = { guard, trace, pipe, compose, composeMethod, partial, spreadArgs };
